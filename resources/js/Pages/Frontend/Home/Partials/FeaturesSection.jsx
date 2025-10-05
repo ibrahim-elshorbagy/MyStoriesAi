5b8f3a0ac48504747248940d52b8302fd6890364
ibrahim elshorbagy <e.mohmed55@gmail.com>
2025-10-06 00:09:32 +0300

    footer

--- resources/js/Pages/Frontend/Home/Partials/FeaturesSection.jsx
+++ resources/js/Pages/Frontend/Home/Partials/FeaturesSection.jsx
@@ -14,10 +14,10 @@ export default function FeaturesSection() {
         {/* Section Header */}
         <div className="text-center mx-auto flex flex-col gap-4">
           <div className="flex items-center justify-center flex-col gap-1">
-            <h2 className="text-4xl xl:text-6xl leading-normal font-bold bg-gradient-to-r from-neutral-700 to-orange-600 bg-clip-text text-transparent">
+            <h2 className="text-4xl pb-2 xl:text-6xl leading-normal font-bold bg-gradient-to-r from-neutral-700 to-orange-600 bg-clip-text text-transparent">
               {t('features_title')}
             </h2>
-            <h3 className="text-2xl xl:text-3xl font-semibold text-orange-700">
+            <h3 className="text-2xl xl:text-3xl font-semibold text-orange-700 mt-4">
               {t('features_subtitle')}
             </h3>
           </div>
