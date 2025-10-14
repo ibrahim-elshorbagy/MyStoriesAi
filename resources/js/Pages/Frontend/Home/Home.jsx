89718ffc9c84a7f3cfee059121bd75897949e335
ibrahim elshorbagy <e.mohmed55@gmail.com>
2025-10-14 12:57:35 +0300

    delete

--- resources/js/Pages/Frontend/Home/Home.jsx
+++ resources/js/Pages/Frontend/Home/Home.jsx
@@ -3,8 +3,6 @@ import { Head } from '@inertiajs/react';
 import React from 'react';
 import HeroSection from './Partials/HeroSection';
 import FeaturesSection from './Partials/FeaturesSection';
-import ContactSection from './Partials/ContactSection';
-import AboutSection from './Partials/AboutSection';
 import StoriesSection from './Partials/StoriesSection.jsx';
 import PricingSection from './Partials/PricingSection';
 import FAQs from './Partials/FAQs';
@@ -19,7 +17,7 @@ export default function Home({ stories, faqs }) {
 
       <HeroSection />
       <FeaturesSection />
-      {/* <StoriesSection stories={stories} /> */}
+      <StoriesSection stories={stories} />
       <PricingSection />
       <FAQs faqs={faqs} />
     </SiteLayout>
