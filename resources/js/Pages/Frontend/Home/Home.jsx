94622b2a9a1d54fa910c2e326428cc314b7a517f
ibrahim elshorbagy <e.mohmed55@gmail.com>
2025-10-24 02:04:20 +0300

    section

--- resources/js/Pages/Frontend/Home/Home.jsx
+++ resources/js/Pages/Frontend/Home/Home.jsx
@@ -1,5 +1,5 @@
 import SiteLayout from '@/Layouts/SiteLayout/SiteLayout';
-import { Head } from '@inertiajs/react';
+import { Head, usePage } from '@inertiajs/react';
 import React from 'react';
 import HeroSection from './Partials/HeroSection';
 import FeaturesSection from './Partials/FeaturesSection';
@@ -10,7 +10,8 @@ import FAQs from './Partials/FAQs';
 import { useTrans } from '@/Hooks/useTrans';
 
 export default function Home({ stories, faqs, categories, settings }) {
-    const { t } = useTrans();
+  const { t } = useTrans();
+  const { locale } = usePage().props;
 
   return (
     <SiteLayout>
@@ -19,6 +20,10 @@ export default function Home({ stories, faqs, categories, settings }) {
       <HeroSection />
       <FeaturesSection />
       <StoriesSection stories={stories} />
+
+      {locale === 'en' && <img className="object-contain " src="/assets/home/1.png" alt="" />}
+      {locale === 'ar' && <img className="object-contain " src="/assets/home/2.png" alt="" />}
+
       <AgeCategoriesSection categories={categories} />
       <PricingSection settings={settings} />
       <FAQs faqs={faqs} />
