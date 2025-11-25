b16d39c80b0dd255d685e86611e7b69ab2832039
ibrahim elshorbagy <e.mohmed55@gmail.com>
2025-11-25 18:07:03 +0200

    rrebuild

--- routes/userBackendOrder.php
+++ routes/userBackendOrder.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\User\Order\UserOrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/user/orders', [UserOrderController::class, 'index'])->name('user.orders.index');
-    Route::get('/user/orders/{order}', [UserOrderController::class, 'show'])->name('user.orders.show');
+Route::middleware(['auth'])->prefix('/dashboard')->group(function () {
+    Route::get('/client/orders', [UserOrderController::class, 'index'])->name('user.orders.index');
+    Route::get('/client/orders/{order}', [UserOrderController::class, 'show'])->name('user.orders.show');
 });
