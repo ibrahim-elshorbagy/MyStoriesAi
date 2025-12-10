e898729271f7bf48fc5bd1414c28bbaf24cb0f48
ibrahim elshorbagy <e.mohmed55@gmail.com>
2025-12-10 18:17:22 +0200

    rebuild

--- resources/js/Pages/Frontend/Home/Partials/AgeCategoriesSection.jsx
+++ resources/js/Pages/Frontend/Home/Partials/AgeCategoriesSection.jsx
@@ -13,7 +13,7 @@ export default function AgeCategoriesSection({ categories }) {
             {t('browse_stories_by_age')}
           </h2>
         </div>
-        <div className="grid grid-cols-2 sm:grid-cols-4 gap-6 items-center">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6 items-center">
           {categories.map((category) => (
             <Link
               key={category.id}
