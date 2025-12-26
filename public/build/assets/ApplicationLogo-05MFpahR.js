96cb151fe4412ffa6744ec19f948155b6938af6d
ibrahim elshorbagy <e.mohmed55@gmail.com>
2025-12-26 19:25:11 +0200

    rebuild

--- public/build/assets/ApplicationLogo-05MFpahR.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-Dz-NXg7s.js";function e(t){return s.jsx(s.Fragment,{children:s.jsx("img",{className:"object-contain ",src:"/assets/settings/logo.webp",alt:""})})}export{e as A};
